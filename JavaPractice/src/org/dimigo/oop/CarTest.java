7b5e7f3639460919e1651156a0e1fe49f1793cd5
user <user@sec>
2015-04-16 12:26:00 +0900

    7-2 과제제출 합니다

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -4,27 +4,27 @@ public class CarTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Car car = new Car();
-		Car car2 = new Car();
-		Car car3 = new Car();
+		Car2 car = new Car2("현대자동차","제네시스","겅정색",225, 50000000);
+		Car2 car2 = new Car2("기아자동차", "K7", "흰색", 246, 40000000);
+		Car2 car3 = new Car2("삼성자돝차", "SM7", "흰색", 200, 38000000);
 		System.out.println("<< 자동차 목록>>");
-		car.setCompany("현대자동차");
-		car.setModel("제네시스");
-		car.setColor("검정색");
-		car.setMaxSpeed(225);
-		car.setPrice(50000000);
-		
-		car2.setCompany("기아자동차");
-		car2.setModel("K7");
-		car2.setColor("흰색");
-		car2.setMaxSpeed(246);
-		car2.setPrice(40000000);
-		
-		car3.setCompany("삼성자동차");
-		car3.setModel("SM7");
-		car3.setColor("회색");
-		car3.setMaxSpeed(200);
-		car3.setPrice(38000000);
+//		car.setCompany("현대자동차");
+//		car.setModel("제네시스");
+//		car.setColor("검정색");
+//		car.setMaxSpeed(225);
+//		car.setPrice(50000000);
+//		
+//		car2.setCompany("기아자동차");
+//		car2.setModel("K7");
+//		car2.setColor("흰색");
+//		car2.setMaxSpeed(246);
+//		car2.setPrice(40000000);
+//		
+//		car3.setCompany("삼성자동차");
+//		car3.setModel("SM7");
+//		car3.setColor("회색");
+//		car3.setMaxSpeed(200);
+//		car3.setPrice(38000000);
 		
 		System.out.println("제조사명 : "+car.getCompany());
 		System.out.println("모델명 : "+car.getModel());
